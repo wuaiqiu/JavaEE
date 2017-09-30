7d6cee4c51c1b510757bca168864044d9aa6ad80
wuaiqiu <209115576@qq.com>
2017-09-30 22:03:04 +0800

    20170930

--- Base/JavaArray.java
+++ Base/JavaArray.java
@@ -1,45 +1,34 @@
-package Base;
+package day4;
 
+import java.util.Arrays;
 
 /*
  *数组定义
- *	a.声明数组  int[] arr 或 int arr[]
- *	b.数组赋值   int[] arr=new int[4] 或 int[] arr={1,2,3}
+ *	
+ *	a.声明数组 int[] arr 或 int arr[]
+ *	b.数组赋值 int[] arr=new int[4] 或 int[] arr={1,2,3}
  *	c.数组遍历 for(int p:arr){}
  *	d.多维数组 int[][] arr=new int[4][5];
  *	e.数组可以作为函数的参数
  *	f.数组可以作为函数的返回值
- *
+ *	g.Arrays方法
+ *	  public static int binarySearch(Object[] a, Object key):用二分查找算法在给定数组中搜索给定值的对象(Byte,Int,double等)
+ *	  public static boolean equals(long[] a, long[] a2):如果两个指定的 long 型数组彼此相等，则返回 true
+ *	  public static void sort(Object[] a):对指定对象数组根据其元素的自然顺序进行升序排列	
+ *	  public static void fill(int[] a, int val):用val值填充a数组
  * */
 
-public class JavaArray {
-		
+public class JavaArray {	
+
 	public static void main(String[] args) {
-			
-			//声明数组
-			int[] arr1=new int[4];
-			int[] arr2= {1,2,3,4};
-
-			//foreach遍历
-			for(int p:arr2) {
-				System.out.println(p);
-			}
-	}
-
-	//值传递，不会影响原数组
-	public  void printArray(int[] array) {
-		  for (int i = 0; i < array.length; i++) {
-		    System.out.print(array[i] + " ");
-		  }
+			int[] arr1= {3,2,7,1};
+			Arrays.sort(arr1);    		//{1,2,3,7}
+			int[] arr2=new int[4];
+			Arrays.fill(arr2, 1);		//{1,1,1,1}
 	}
 	
+	public int[] function(int[] arr) {
+			return null;
+	}
 	
-	public static int[] reverse(int list) {
-		  int[] result = new int[list];
-		 
-		  for (int i = 0, j = result.length - 1; i < list; i++, j--) {
-		    result[j] = i;
-		  }
-		  return result;
-		 } 
 }
--- Base/JavaDataType.java
+++ Base/JavaDataType.java
@@ -1,25 +1,32 @@
-package Base;
+package day4;
 /*
- * 基本数据类型
- * 	byte(8)	short(16)	int(32)	  long(64)	float(32)	double(64)
+ *(1)基本数据类型
+ * 	byte(8)	short(16) int(32) long(64) float(32) double(64)
  * 	boolean	char(16)
  * 	a.char字符需要单引号
  * 	b.前缀0表示8进制，而前缀0x代表16进制	
  * 	c.long类型需要在字面量后面加'L'，float类型需要在字面量后面加'f'
- * 	d.byte short int long默认值为0，float double默认值为0.0，boolean默认值为false，char默认值为Unicode码0
+ * 	d.byte short int long默认值为0，float double默认值为0.0，boolean默认值为false，char默认值为ASCII码0
  * 	e.基本数据类型在全局时才有默认值
  * 
- * 引用类型
+ *(2)引用类型
  * 	a.对象、数组都是引用数据类型。
- *  	b.所有引用类型的默认值都是null。
- *  	c.引用类型在全局时才有默认值
+ *  b.所有引用类型的默认值都是null。
+ *  c.引用类型在全局时才有默认值
  *  
- * 自动类型转换
- * 		a.不能对boolean类型进行类型转换。
- * 		b.不能把对象类型转换成不相关类的对象。
- * 		c.在把容量大的类型转换为容量小的类型时必须使用强制类型转换。
- * 		d.浮点数到整数的转换是通过舍弃小数得到，而不是四舍五入。
+ *(3)自动类型转换
+ * 	a.不能对boolean类型进行类型转换。
+ * 	b.不能把对象类型转换成不相关类的对象。
+ * 	c.在把容量大的类型转换为容量小的类型时必须使用强制类型转换。
+ * 	d.浮点数到整数的转换是通过舍弃小数得到，而不是四舍五入。
  *  
+ *(4)值传递与引用传递
+ *   a.基本数据类型传值，对形参的修改不会影响实参；
+ *   b.引用类型传引用，形参和实参指向同一个内存地址（同一个对象），所以对参数的修改会影响到实际的对象；
+ *   c.String, Integer, Double等immutable的类型特殊处理，可以理解为传值，最后的操作不会修改实参对象。
+ *   
+ *(5)引用类型判断instanceof
+ *	obj instanceof class :判断obj是否为class的实例
  * */
 
 public class JavaDataType {
@@ -27,6 +34,7 @@ public class JavaDataType {
 	
 	public static void main(String[] args) {
 		
+		//基本数据类型
 		char a='d';
 		long b=12L;
 		float c=2.2f;
@@ -35,9 +43,20 @@ public class JavaDataType {
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println((int)d);
-		System.out.println("---");
+		
+		//类型判断
+		JavaDataType obj=new JavaDataType();
+		System.out.println(obj instanceof JavaDataType);
 		
 		
+		//值传递
+		String str1="Hello";
+		String str2=new String("Hello");
+		fun(str1);//Hello
+		fun(str2);//Hello
 	}
-
+	
+	public static void fun(String name) {
+		name="hello";
+	}
 }
